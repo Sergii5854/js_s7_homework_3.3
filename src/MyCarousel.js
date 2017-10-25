fa2177748688eace184b44cdebad0c4e90fb2258
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-10-25 08:47:54 +0300

    add template from lib

--- src/MyCarousel.js
+++ src/MyCarousel.js
@@ -1,1 +1,42 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
+
+var ReactDOM = require('react-dom');
+var Carousel = require('react-responsive-carousel').Carousel;
+
+var DemoCarouselVertical = React.createClass({
+    render() {
+        return (
+            <Carousel axis="vertical" showArrows={true} onChange={onChange} onClickItem={onClickItem} onClickThumb={onClickThumb}>
+                <div>
+                    <img src="assets/1.jpeg" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="assets/2.jpeg" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="assets/3.jpeg" />
+                    <p className="legend">Legend 3</p>
+                </div>
+                <div>
+                    <img src="assets/4.jpeg" />
+                    <p className="legend">Legend 4</p>
+                </div>
+                <div>
+                    <img src="assets/5.jpeg" />
+                    <p className="legend">Legend 5</p>
+                </div>
+                <div>
+                    <img src="assets/6.jpeg" />
+                    <p className="legend">Legend 6</p>
+                </div>
+            </Carousel>
+        );
+    }
+});
+ReactDOM.render(<DemoCarouselVertical />, document.querySelector('.demo-carousel-vertical'));
+
+// Don't forget to include the css in your page 
+// <link rel="stylesheet" href="carousel.css"/>
