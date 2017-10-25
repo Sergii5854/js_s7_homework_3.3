833fcb0e87b38e14bcbb37585d7f14bfcc1ff45d
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-10-25 09:07:41 +0300

    add another lib with css

--- src/MyCarousel.js
+++ src/MyCarousel.js
@@ -44,26 +44,7 @@ export default class MyCarousel extends Component {
                     <img src="http://lorempixel.com/output/cats-q-c-640-480-9.jpg" />
                     <p className="legend">Legend 9</p>
                 </div>
-                <div>
-                    <img src="http://lorempixel.com/output/cats-q-c-640-480-10.jpg" />
-                    <p className="legend">Legend 10</p>
-                </div>
-                <div>
-                    <img src="http://lorempixel.com/output/cats-q-c-640-480-11.jpg" />
-                    <p className="legend">Legend 11</p>
-                </div>
-                <div>
-                    <img src="http://lorempixel.com/output/cats-q-c-640-480-12.jpg" />
-                    <p className="legend">Legend 12</p>
-                </div>
-                <div>
-                    <img src="http://lorempixel.com/output/cats-q-c-640-480-13.jpg" />
-                    <p className="legend">Legend 13</p>
-                </div>
-                <div>
-                    <img src="http://lorempixel.com/output/cats-q-c-640-480-14.jpg" />
-                    <p className="legend">Legend 14</p>
-                </div>
+       
             </Carousel>
         );
     }
